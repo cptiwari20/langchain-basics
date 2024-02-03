2a98e7a444b976e9a57cbed62819ee9e9c7a646d
Chandra <cptiwari20@gmail.com>
2024-02-03 09:19:24 +0530

    Added memory to the chain

--- chat-cli.py
+++ chat-cli.py
@@ -1,24 +1,32 @@
 from langchain.chains import LLMChain
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
 from langchain_openai import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
 from dotenv import load_dotenv
 
 load_dotenv()
 
 chat = ChatOpenAI()
 
+memory = ConversationBufferMemory(memory_key="message", return_messages=True)
+#memory key is the object key that we want to assign to the conversational data.
+#return messages is for returning the previous messages in the memory so that we can reuse for the next conversation.
+
 prompt = ChatPromptTemplate(
-    input_variables=["content"],
+    input_variables=["content", "message"], #content is from the user input and message from the memory
     messages=[
+        MessagesPlaceholder(variable_name="message"), #variable name here is coming from the memory, the memory key that we had put
         HumanMessagePromptTemplate.from_template("{content}")
     ]
 )
 
 chain = LLMChain(
     llm=chat,
-    prompt=prompt
+    prompt=prompt,
+    memory=memory 
 )
 
+print("Please ask whatever you want to ask with the OpenAI Chatbot!")
 while True:
     content = input(">>> ")
     response = chain({"content": content})
