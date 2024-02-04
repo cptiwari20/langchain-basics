fab811cbcc811f365ece9a14ef4dc5d8fbc1e21d
Chandra <cptiwari20@gmail.com>
2024-02-04 10:05:24 +0530

    Added summary based memory

--- chat-cli.py
+++ chat-cli.py
@@ -1,20 +1,26 @@
 from langchain.chains import LLMChain
 from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
 from langchain_openai import ChatOpenAI
-from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory, ConversationSummaryMemory
 from dotenv import load_dotenv
 
 load_dotenv()
 
 chat = ChatOpenAI()
 
-memory = ConversationBufferMemory(
-    chat_memory=FileChatMessageHistory('message.json'),
-    memory_key="message", 
-    return_messages=True
-    )
+# memory = ConversationBufferMemory(
+#     chat_memory=FileChatMessageHistory('message.json'), #this will store the history in file
+#     memory_key="message", 
+#     return_messages=True
+#     )
 #memory key is the object key that we want to assign to the conversational data.
 #return messages is for returning the previous messages in the memory so that we can reuse for the next conversation.
+
+memory = ConversationSummaryMemory(
+    llm=chat,
+    memory_key="message",
+    return_messages=True
+)
 
 prompt = ChatPromptTemplate(
     input_variables=["content", "message"], #content is from the user input and message from the memory
