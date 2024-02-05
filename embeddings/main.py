29596f98d6e1ce3176ddf8daacd5c3570c3902ad
Chandra <cptiwari20@gmail.com>
2024-02-05 09:16:14 +0530

    improved spliiting

--- embeddings/main.py
+++ embeddings/main.py
@@ -1,5 +1,5 @@
-from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,19 +7,19 @@ load_dotenv()
 # import the file and then split into chunks
 # chunks should be in limited lenght and should be chunked based on line change
 text_splitter = CharacterTextSplitter(
+    separator="\n",
     chunk_size=200,
     chunk_overlap=0,
-    keep_separator="/n"
 )
 
 # Load the file 
-loader = TextLoader('facts.txt')
+loader = TextLoader("facts.txt")
 docs = loader.load_and_split(
     text_splitter=text_splitter
 )
 
 for doc in docs:
-    print(doc)
-    print("/n")
+    print(doc.page_content)
+    print("\n")
 
 # take those chunks and create embeddings
