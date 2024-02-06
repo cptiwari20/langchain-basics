28353ba594fc47d4da09da6d35b7b53727905258
Chandra <cptiwari20@gmail.com>
2024-02-06 09:42:43 +0530

    Added chromadb with embeddings

--- embeddings/main.py
+++ embeddings/main.py
@@ -1,8 +1,12 @@
 from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
 from dotenv import load_dotenv
 
 load_dotenv()
+
+embeddings = OpenAIEmbeddings()
 
 # import the file and then split into chunks
 # chunks should be in limited lenght and should be chunked based on line change
@@ -18,8 +22,17 @@ docs = loader.load_and_split(
     text_splitter=text_splitter
 )
 
-for doc in docs:
-    print(doc.page_content)
-    print("\n")
+db = Chroma.from_documents(
+    docs,
+    embedding_function=embeddings,
+    persist_directory='emb'
+)
+
+result = db.similarity_search_with_score("What is english?")
+
+print(result)
+# for doc in docs:
+#     print(doc.page_content)
+#     print("\n")
 
 # take those chunks and create embeddings
