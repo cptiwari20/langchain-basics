cf9a2314628e8d8006f6de3a65d93ef071d998b7
Chandra <cptiwari20@gmail.com>
2024-02-15 08:36:16 +0530

    Added Memory to the agent

--- agents/main.py
+++ agents/main.py
@@ -6,6 +6,7 @@ from langchain.prompts import (
     HumanMessagePromptTemplate,
 )
 from langchain.schema import SystemMessage
+from langchain.memory import ConversationBufferMemory
 from dotenv import load_dotenv
 
 from tools.sql import run_query_tool, get_tables, run_describe_tables
@@ -14,6 +15,7 @@ from tools.report import write_report_tool
 load_dotenv()
 
 chat = ChatOpenAI()
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
 tables = get_tables()
 prompt = ChatPromptTemplate(
@@ -26,6 +28,7 @@ prompt = ChatPromptTemplate(
 
             )
         ),
+        MessagesPlaceholder(variable_name="chat_history"), # here beacause want to execute before the human message
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad") 
         #agent_scratchpad is a kind of memory that is comping from the function call by the agents
@@ -43,7 +46,9 @@ agent = OpenAIFunctionsAgent(
 agent_executor = AgentExecutor(
     agent=agent,
     tools=tools,
+    memory=memory,
     verbose=True #for printing the output in detail
 )
 
-agent_executor("How many products are there in th database, generate a report for this with pricing.")
+agent_executor("How many products are there in th database?")
+agent_executor("find same for users")
